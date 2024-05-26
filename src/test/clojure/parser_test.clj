802c141bd8ac77660499171f26022fae9644c840
Denis Perdomo <joanp307@gmail.com>
2024-05-25 22:26:38 -0400

    reformatted the number-related tests

--- src/test/clojure/parser_test.clj
+++ src/test/clojure/parser_test.clj
@@ -60,31 +60,71 @@
 
 ;;; NUMBERS
 
-(deftest positive-integers
-  (are [src] (= (parse src)
-                (list [:number
-                       [:prefix10 [:exactness] [:radix10]]
-                       [:complex10 [:real10 [:sign] [:ureal10 (reduce conj
-                                                                      [:uinteger10]
-                                                                      (map str src))]]]]))
+(deftest uinteger2
+  (are [src] (= (parse src :starting-at :uinteger2)
+                (reduce conj [:uinteger2] (map str src)))
     "0"
-    "1"
-    "123456789"))
+    "0000"
+    "0001"
+    "1000"
+    "1111")
+  (are [src] (failure? (parse src :starting-at :uinteger2))
+    "-012"
+    "012"
+    "210"
+    "a0b"))
 
-(deftest negative-integers
-  (are [src] (= (parse src)
-                (list [:number
-                       [:prefix10 [:exactness] [:radix10]]
-                       [:complex10 [:real10 [:sign "-"] [:ureal10 (reduce conj
-                                                                          [:uinteger10]
-                                                                          (map str (rest src)))]]]]))
-    "-0"
-    "-1"
-    "-123456789"))
+(deftest uinteger8
+  (are [src] (= (parse src :starting-at :uinteger8)
+                (reduce conj [:uinteger8] (map str src)))
+    "01234567"
+    "000000011"
+    "76543210")
+  (are [src] (failure? (parse src :starting-at :uinteger8))
+    "-01234567"
+    "0123456789"
+    "0123456789abcdef"))
+
+(deftest uinteger10
+  (are [src] (= (parse src :starting-at :uinteger10)
+                (reduce conj [:uinteger10] (map str src)))
+    "0123456789"
+    "000000011"
+    "9876543210")
+  (are [src] (failure? (parse src :starting-at :uinteger10))
+    "0123456789a"
+    "-0123456789"
+    "0123456789abcdef"))
+
+(deftest uinteger16
+  (are [src] (= (parse src :starting-at :uinteger16)
+                (reduce conj [:uinteger16] (map str src)))
+    "0123456789abcdef"
+    "0011"
+    "fedcba9876543210")
+  (are [src] (failure? (parse src :starting-at :uinteger16))
+    "0123456789abcdefg"
+    "-0011"))
+
+(deftest number-prefixes
+  (is (= (parse "#b#i" :starting-at :prefix2) [:prefix2 [:radix2 "#b"] [:exactness "#i"]]))
+  (is (= (parse "#b" :starting-at :prefix2) [:prefix2 [:exactness] [:radix2 "#b"]]))
+  (is (= (parse "#E" :starting-at :prefix10) [:prefix10 [:exactness "#E"] [:radix10]]))
+  (is (= (parse "#E#d" :starting-at :prefix10) [:prefix10 [:exactness "#E"] [:radix10 "#d"]])))
+
+(deftest number-suffixes
+  (is (= (parse "" :starting-at :suffix) [:suffix]))
+  (let [src "d+16"]
+    (is (= (parse src :starting-at :suffix) [:suffix [:exponent-marker "d"] [:sign "+"] "1" "6"])))
+  (is (failure? (parse "L" :starting-at :suffix))))
 
 (deftest numbers
-  (positive-integers)
-  (negative-integers))
+  (uinteger2)
+  (uinteger8)
+  (uinteger10)
+  (uinteger16)
+  (number-prefixes)
+  (number-suffixes))
 
 (defn test-ns-hook []
   (comment-successes)
