5df5275d43bfbc329989981eb1d3ec7abede83a7
Denis Perdomo <joanp307@gmail.com>
2024-06-30 00:34:29 -0400

    Decimal Literal Node in Clojure

--- src/main/clojure/truffle_scheme6/parser_types.clj
+++ src/main/clojure/truffle_scheme6/parser_types.clj
@@ -3,10 +3,6 @@
   (:import (truffle_scheme6.nodes.atoms.bools SFalseLiteralNode STrueLiteralNode)
            (truffle_scheme6.nodes.atoms.numbers SExactIntegerNode SExactRealNode SFractionLiteralNode SInexactIntegerNode SInexactReal32Node SInexactReal64Node)))
 
-(defn- parse-float
-  [s]
-  (Float/parseFloat s))
-
 (defprotocol PSchemeNode
   (to-java [this] "Transforms a given node to a Java object"))
 
@@ -20,19 +16,29 @@
   (to-java [this]
     (STrueLiteralNode.)))
 
-(defrecord IntegerLiteral [exact? sign uint-str]
+(defrecord IntegerLiteral [exact? radix sign uint-str]
   PSchemeNode
   (to-java [this]
     (let [val (if exact?
-                (SExactIntegerNode. (BigInteger. ^String uint-str))
-                (SInexactIntegerNode. (parse-long uint-str)))]
+                (SExactIntegerNode. (BigInteger. ^String uint-str ^int radix))
+                (SInexactIntegerNode. (Long/parseLong uint-str radix)))]
       (if (= sign "-")
         (.negate val)
         val))))
 
-(defrecord FractionLiteral [exact? sign numerator-str denominator-str]
+(defrecord FractionLiteral [exact? radix sign numerator-str denominator-str]
   PSchemeNode
   (to-java [this]
-    (let [numerator (->IntegerLiteral exact? sign numerator-str)
-          denominator (->IntegerLiteral exact? "+" denominator-str)]
+    (let [numerator (->IntegerLiteral exact? radix sign numerator-str)
+          denominator (->IntegerLiteral exact? radix "+" denominator-str)]
       (SFractionLiteralNode. (to-java numerator) (to-java denominator)))))
+
+(defrecord DecimalLiteral [exact? sign decimal-str exp-mark exp-val mantissa-width] ; mantissa is ignored for now
+  PSchemeNode
+  (to-java [this]
+    (let [val (cond exact? (SExactRealNode. ^BigDecimal (BigDecimal. ^String decimal-str))
+                    (some #{"s" "S" "f" "F"} [exp-mark]) (SInexactReal32Node. (Float/parseFloat decimal-str))
+                    :else (SInexactReal64Node. (Double/parseDouble decimal-str)))
+          val (if exp-val (.pow val exp-val) val)
+          val (if (= sign "-") (.negate val) val)]
+      val)))
