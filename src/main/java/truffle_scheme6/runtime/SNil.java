71cffab31d8c0523a6b503ed722bb3436c7a8658
Denis Perdomo <joanp307@gmail.com>
2024-10-10 12:45:44 -0400

    interop library implemented for runtime.SNil

--- src/main/java/truffle_scheme6/runtime/SNil.java
+++ src/main/java/truffle_scheme6/runtime/SNil.java
@@ -1,9 +1,26 @@
 package truffle_scheme6.runtime;
 
-public class SNil {
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+
+@ExportLibrary(InteropLibrary.class)
+public class SNil implements TruffleObject {
     public static final SNil SINGLETON = new SNil();
 
-    private SNil() {}
+    private SNil() {
+    }
+
+    @ExportMessage
+    boolean isNull() {
+        return true;
+    }
+
+    @ExportMessage
+    Object toDisplayString(boolean allowSideEffects) {
+        return this.toString();
+    }
 
     @Override
     public String toString() {
