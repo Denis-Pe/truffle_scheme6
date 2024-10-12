6e21dfe5f8c32083d65876806e285246cfac5a35
Denis Perdomo <joanp307@gmail.com>
2024-10-11 23:27:40 -0400

    interop library implemented for runtime.SByteVector

--- src/main/java/truffle_scheme6/runtime/SByteVector.java
+++ src/main/java/truffle_scheme6/runtime/SByteVector.java
@@ -1,20 +1,103 @@
 package truffle_scheme6.runtime;
 
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.InvalidArrayIndexException;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
 import truffle_scheme6.utils.StringFormatting;
 
-public class SByteVector {
-    private final byte[] value;
+import java.util.Iterator;
 
-    public SByteVector(byte[] value) {
-        this.value = value;
+@ExportLibrary(InteropLibrary.class)
+public class SByteVector implements TruffleObject {
+    private final byte[] bytes;
+
+    public SByteVector(byte[] bytes) {
+        this.bytes = bytes;
     }
 
-    public byte[] getValue() {
-        return value;
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    @ExportMessage
+    boolean hasArrayElements() {
+        return true;
+    }
+
+    @ExportMessage
+    long getArraySize() {
+        return bytes.length;
+    }
+
+    @ExportMessage
+    boolean isArrayElementReadable(long index) {
+        return 0 <= index && index < bytes.length;
+    }
+
+    @ExportMessage
+    Object readArrayElement(long index) throws InvalidArrayIndexException {
+        try {
+            return bytes[Math.toIntExact(index)];
+        } catch (IndexOutOfBoundsException | ArithmeticException e) {
+            throw InvalidArrayIndexException.create(index);
+        }
+    }
+
+    @ExportMessage
+    boolean isArrayElementModifiable(long index) {
+        return isArrayElementReadable(index);
+    }
+
+    @ExportMessage
+    void writeArrayElement(long index, Object value) throws UnsupportedTypeException, InvalidArrayIndexException {
+        try {
+            bytes[Math.toIntExact(index)] = (Byte) value;
+        } catch (IndexOutOfBoundsException | ArithmeticException e) {
+            throw InvalidArrayIndexException.create(index);
+        } catch (ClassCastException e) {
+            throw UnsupportedTypeException.create(new Object[]{value}, "Value given is not a byte");
+        }
+    }
+
+    @ExportMessage
+    boolean isArrayElementInsertable(long index) {
+        return false;
+    }
+
+    @ExportMessage
+    boolean hasIterator() {
+        return true;
+    }
+
+    @ExportMessage
+    Object getIterator() {
+        // there's no easy way to do it with byte[] :)
+        return new Iterator<Byte>() {
+            byte[] elms = bytes;
+            int i = 0;
+
+            @Override
+            public boolean hasNext() {
+                return 0 <= i && i < elms.length;
+            }
+
+            @Override
+            public Byte next() {
+                return elms[i++];
+            }
+        };
+    }
+
+    @ExportMessage
+    String toDisplayString(boolean allowSideEffects) {
+        return this.toString();
     }
 
     @Override
     public String toString() {
-        return "#vu8(" + StringFormatting.spaced(value) + ")";
+        return "#vu8(" + StringFormatting.spaced(bytes) + ")";
     }
 }
