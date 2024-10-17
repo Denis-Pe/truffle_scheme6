1e53e550f17da24c60aabc4a61f5f51e1bd5735f
Denis Perdomo <joanp307@gmail.com>
2024-10-16 23:10:21 -0400

    implemented writing of different types of slots in the let node

--- src/main/java/truffle_scheme6/nodes/special/SLetNode.java
+++ src/main/java/truffle_scheme6/nodes/special/SLetNode.java
@@ -1,5 +1,6 @@
 package truffle_scheme6.nodes.special;
 
+import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.NodeField;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -15,12 +16,63 @@ public class SLetNode extends SSpecialNode {
     public static abstract class SLetBindingNode extends SSpecialNode {
         protected abstract int getSlot();
 
-        @Specialization
+        protected final static FrameSlotKind BOOLEAN = FrameSlotKind.Boolean;
+        protected final static FrameSlotKind LONG = FrameSlotKind.Long;
+        protected final static FrameSlotKind FLOAT = FrameSlotKind.Float;
+        protected final static FrameSlotKind DOUBLE = FrameSlotKind.Double;
+        protected final static FrameSlotKind BYTE = FrameSlotKind.Byte;
+
+        @Specialization(guards = "isKindOrIllegal(frame, BOOLEAN)")
+        protected Object setBoolean(VirtualFrame frame, boolean value) {
+            frame.getFrameDescriptor().setSlotKind(getSlot(), FrameSlotKind.Boolean);
+
+            frame.setBoolean(getSlot(), value);
+            return value;
+        }
+
+        @Specialization(guards = "isKindOrIllegal(frame, LONG)")
+        protected Object setLong(VirtualFrame frame, long value) {
+            frame.getFrameDescriptor().setSlotKind(getSlot(), FrameSlotKind.Long);
+
+            frame.setLong(getSlot(), value);
+            return value;
+        }
+
+        @Specialization(guards = "isKindOrIllegal(frame, FLOAT)")
+        protected Object setFloat(VirtualFrame frame, float value) {
+            frame.getFrameDescriptor().setSlotKind(getSlot(), FrameSlotKind.Float);
+
+            frame.setFloat(getSlot(), value);
+            return value;
+        }
+
+        @Specialization(guards = "isKindOrIllegal(frame, DOUBLE)")
+        protected Object setDouble(VirtualFrame frame, double value) {
+            frame.getFrameDescriptor().setSlotKind(getSlot(), FrameSlotKind.Double);
+
+            frame.setDouble(getSlot(), value);
+            return value;
+        }
+
+        @Specialization(guards = "isKindOrIllegal(frame, BYTE)")
+        protected Object setByte(VirtualFrame frame, byte value) {
+            frame.getFrameDescriptor().setSlotKind(getSlot(), FrameSlotKind.Byte);
+
+            frame.setByte(getSlot(), value);
+            return value;
+        }
+
+        @Specialization(replaces = {"setBoolean", "setLong", "setFloat", "setDouble", "setByte"})
         protected Object setObject(VirtualFrame frame, Object value) {
             frame.getFrameDescriptor().setSlotKind(getSlot(), FrameSlotKind.Object);
 
             frame.setObject(getSlot(), value);
             return value;
+        }
+
+        protected boolean isKindOrIllegal(VirtualFrame frame, FrameSlotKind primitiveKind) {
+            FrameSlotKind kind = frame.getFrameDescriptor().getSlotKind(getSlot());
+            return kind == primitiveKind || kind == FrameSlotKind.Illegal;
         }
 
         public abstract Object execute(VirtualFrame frame);
