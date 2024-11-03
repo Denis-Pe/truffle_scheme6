50ce9739f4afc1b6799a600b389fcad06ef1f0b7
Denis Perdomo <joanp307@gmail.com>
2024-11-03 00:01:49 -0400

    read arg nodes now inherit from SchemeNode and hence share type systems

--- src/main/java/truffle_scheme6/nodes/functions/SReadArgNode.java
+++ src/main/java/truffle_scheme6/nodes/functions/SReadArgNode.java
@@ -2,8 +2,14 @@ package truffle_scheme6.nodes.functions;
 
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.Node;
+import truffle_scheme6.SchemeNode;
 
-public abstract class SReadArgNode extends Node {
+public abstract class SReadArgNode extends SchemeNode {
     public abstract Object execute(VirtualFrame frame);
+
+    @Override
+    public final Object executeFrozen(VirtualFrame frame) {
+        throw new UnsupportedOperationException("Cannot freeze read argument node");
+    }
 }
 
