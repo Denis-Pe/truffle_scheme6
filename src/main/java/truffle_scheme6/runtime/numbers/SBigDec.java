a8abbc79822e6e6616ec39474c74bc50de550b78
Denis Perdomo <joanp307@gmail.com>
2024-11-20 22:34:11 -0500

    made my bigint and bigdec box types implement TruffleObject and Interop

--- src/main/java/truffle_scheme6/runtime/numbers/SBigDec.java
+++ src/main/java/truffle_scheme6/runtime/numbers/SBigDec.java
@@ -1,9 +1,16 @@
 package truffle_scheme6.runtime.numbers;
 
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
-public record SBigDec(BigDecimal value) {
+@ExportLibrary(InteropLibrary.class)
+public record SBigDec(BigDecimal value) implements TruffleObject {
     public SBigDec(double value) {
         this(BigDecimal.valueOf(value));
     }
@@ -18,5 +25,87 @@ public record SBigDec(BigDecimal value) {
 
     public SBigDec(SBigInt value) {
         this(value.value());
+    }
+
+    @ExportMessage
+    boolean isNumber() {
+        return true;
+    }
+
+    @ExportMessage
+    boolean fitsInByte() {
+        return false;
+    }
+
+    @ExportMessage
+    boolean fitsInShort() {
+        return false;
+    }
+
+    @ExportMessage
+    boolean fitsInInt() {
+        return false;
+    }
+
+    @ExportMessage
+    boolean fitsInLong() {
+        return false;
+    }
+
+    @ExportMessage
+    boolean fitsInFloat() {
+        return !Float.isInfinite(value.floatValue());
+    }
+
+    @ExportMessage
+    boolean fitsInDouble() {
+        return !Double.isInfinite(value.doubleValue());
+    }
+
+    @ExportMessage
+    byte asByte() throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    short asShort() throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    int asInt() throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    long asLong() throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    float asFloat() throws UnsupportedMessageException {
+        float floatValue = value.floatValue();
+
+        if (Float.isInfinite(floatValue)) {
+            throw UnsupportedMessageException.create();
+        } else {
+            return floatValue;
+        }
+    }
+
+    @ExportMessage
+    double asDouble() throws UnsupportedMessageException {
+        double doubleValue = value.doubleValue();
+
+        if (Double.isInfinite(doubleValue)) {
+            throw UnsupportedMessageException.create();
+        } else {
+            return doubleValue;
+        }
+    }
+
+    @ExportMessage
+    String toDisplayString(boolean sideEffectsAllowed) {
+        return toString();
     }
 }
--- src/main/java/truffle_scheme6/runtime/numbers/SBigInt.java
+++ src/main/java/truffle_scheme6/runtime/numbers/SBigInt.java
@@ -1,11 +1,135 @@
 package truffle_scheme6.runtime.numbers;
 
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+
 import java.math.BigInteger;
 
-public record SBigInt(BigInteger value) {
+@ExportLibrary(InteropLibrary.class)
+public record SBigInt(BigInteger value) implements TruffleObject {
     public SBigInt(long value) {
         this(BigInteger.valueOf(value));
     }
 
+    @ExportMessage
+    public boolean isNumber() {
+        return true;
+    }
 
+    @ExportMessage
+    boolean fitsInByte() {
+        try {
+            value.byteValueExact();
+            return true;
+        } catch (ArithmeticException e) {
+            return false;
+        }
+    }
+
+    @ExportMessage
+    boolean fitsInShort() {
+        try {
+            value.shortValueExact();
+            return true;
+        } catch (ArithmeticException e) {
+            return false;
+        }
+    }
+
+    @ExportMessage
+    boolean fitsInInt() {
+        try {
+            value.intValueExact();
+            return true;
+        } catch (ArithmeticException e) {
+            return false;
+        }
+    }
+
+    @ExportMessage
+    boolean fitsInLong() {
+        try {
+            value.longValueExact();
+            return true;
+        } catch (ArithmeticException e) {
+            return false;
+        }
+    }
+
+    @ExportMessage
+    boolean fitsInFloat() {
+        return !Float.isInfinite(value.floatValue());
+    }
+
+    @ExportMessage
+    boolean fitsInDouble() {
+        return !Double.isInfinite(value.doubleValue());
+    }
+
+    @ExportMessage
+    byte asByte() throws UnsupportedMessageException {
+        try {
+            return value.byteValueExact();
+        } catch (ArithmeticException e) {
+            throw UnsupportedMessageException.create(e);
+        }
+    }
+
+    @ExportMessage
+    short asShort() throws UnsupportedMessageException {
+        try {
+            return value.shortValueExact();
+        } catch (ArithmeticException e) {
+            throw UnsupportedMessageException.create(e);
+        }
+    }
+
+    @ExportMessage
+    int asInt() throws UnsupportedMessageException {
+        try {
+            return value.intValueExact();
+        } catch (ArithmeticException e) {
+            throw UnsupportedMessageException.create(e);
+        }
+    }
+
+    @ExportMessage
+    long asLong() throws UnsupportedMessageException {
+        try {
+            return value.longValueExact();
+        } catch (ArithmeticException e) {
+            throw UnsupportedMessageException.create(e);
+        }
+    }
+
+    @ExportMessage
+    float asFloat() throws UnsupportedMessageException {
+        float floatValue = value.floatValue();
+
+        if (Float.isInfinite(floatValue)) {
+            throw UnsupportedMessageException.create();
+        } else {
+            return floatValue;
+        }
+    }
+
+    @ExportMessage
+    double asDouble() throws UnsupportedMessageException {
+        double doubleValue = value.doubleValue();
+
+        if (Double.isInfinite(doubleValue)) {
+            throw UnsupportedMessageException.create();
+        } else {
+            return doubleValue;
+        }
+    }
+    
+    @ExportMessage
+    String toDisplayString(boolean allowSideEffects) {
+        return toString();
+    }
 }
