000820d7a8e7da5c8a9ee649d6cdcc6f10b8ae8c
Denis Perdomo <joanp307@gmail.com>
2024-12-01 00:18:22 -0500

    testing exact?

---
+++ src/test/java/truffle_scheme6/builtins/SExactTest.java
@@ -0,0 +1,19 @@
+package truffle_scheme6.builtins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SExactTest extends BuiltInTest {
+    @Test
+    void test() {
+        assertTrue(eval("(exact? 0)").asBoolean());
+        assertTrue(eval("(exact? 1)").asBoolean());
+        assertTrue(eval("(exact? 1/2)").asBoolean());
+        assertFalse(eval("(exact? #i1/2)").asBoolean());
+        assertFalse(eval("(exact? 1.2)").asBoolean());
+        assertFalse(eval("(exact? 1.2s10)").asBoolean());
+        assertTrue(eval("(exact? #e1.2L10)").asBoolean());
+        assertFalse(eval("(exact? +inf.0)").asBoolean());
+    }
+}
