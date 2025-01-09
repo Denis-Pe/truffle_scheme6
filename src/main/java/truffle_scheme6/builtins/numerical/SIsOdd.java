11e2c904b34afd2761eababd3be3191218a8a183
Denis Perdomo <joanp307@gmail.com>
2025-01-08 21:40:57 -0500

    fixed odd? for integer-valued fractions

--- src/main/java/truffle_scheme6/builtins/numerical/SIsOdd.java
+++ src/main/java/truffle_scheme6/builtins/numerical/SIsOdd.java
@@ -8,6 +8,8 @@ import truffle_scheme6.annotations.BuiltinInfo;
 import truffle_scheme6.builtins.SBuiltin;
 import truffle_scheme6.nodes.STypesStrong;
 import truffle_scheme6.runtime.numbers.SBigInt;
+import truffle_scheme6.runtime.numbers.SFractionBigInt;
+import truffle_scheme6.runtime.numbers.SFractionLong;
 
 import java.math.BigInteger;
 
@@ -23,5 +25,17 @@ public abstract class SIsOdd extends SBuiltin {
     @Specialization
     public boolean doBigInteger(SBigInt i) {
         return i.value().remainder(BigInteger.TWO).compareTo(BigInteger.ZERO) != 0;
+    }
+
+    @Specialization(rewriteOn = ArithmeticException.class)
+    public boolean doLongFraction(SFractionLong fraction) {
+        var divTwo = fraction.divideExact(new SFractionLong(2));
+        return !divTwo.isPerfectlyDivisible();
+    }
+
+    @Specialization
+    public boolean doBigFraction(SFractionBigInt fraction) {
+        var divTwo = fraction.divide(new SFractionBigInt(BigInteger.TWO));
+        return !divTwo.isPerfectlyDivisible();
     }
 }
