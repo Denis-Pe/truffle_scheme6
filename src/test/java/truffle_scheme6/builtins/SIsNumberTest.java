dbf0f9a646cb2a5e6374a0b352448ae959fb7a7d
Denis Perdomo <joanp307@gmail.com>
2025-01-16 23:51:30 -0500

    testing number?

---
+++ src/test/java/truffle_scheme6/builtins/SIsNumberTest.java
@@ -0,0 +1,19 @@
+package truffle_scheme6.builtins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SIsNumberTest extends BuiltInTest {
+    @Test
+    void test() {
+        assertTrue(eval("(number? 0)").asBoolean());
+        assertTrue(eval("(number? 11111111111111111111111111111111111111111111111111111111111)").asBoolean());
+        assertTrue(eval("(number? 1.0f0)").asBoolean());
+        assertTrue(eval("(number? 2.0d0)").asBoolean());
+        assertTrue(eval("(number? 1/25)").asBoolean());
+        assertTrue(eval("(number? -1/255555555555555555555555555555555555555555555555555555555555)").asBoolean());
+        assertTrue(eval("(number? #e1.2555)").asBoolean());
+        assertTrue(eval("(number? 1/5+i)").asBoolean());
+    }
+}
