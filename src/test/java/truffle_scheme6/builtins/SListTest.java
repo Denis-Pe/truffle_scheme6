20241d8045d291358a5c74520c022053e1af393d
Denis Perdomo <joanp307@gmail.com>
2025-01-27 23:09:27 -0500

    testing list

---
+++ src/test/java/truffle_scheme6/builtins/SListTest.java
@@ -0,0 +1,19 @@
+package truffle_scheme6.builtins;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SListTest extends BuiltInTest {
+    @Test
+    void test() {
+        var a_7_c = eval("(list 'a (+ 3 4) 'c)");
+        assertTrue(a_7_c.hasArrayElements());
+        assertEquals(3L, a_7_c.getArraySize());
+        assertEquals("a", a_7_c.getArrayElement(0).asString());
+        assertEquals(7, a_7_c.getArrayElement(1).asLong());
+        assertEquals("c", a_7_c.getArrayElement(2).asString());
+        
+        assertTrue(eval("(list)").isNull());
+    }
+}
