63ebf6a38bc99aa6f90a8378ecf3582c7779c91f
Denis Perdomo <joanp307@gmail.com>
2026-01-20 20:20:21 -0500

    moved symbol? built-in

--- src/main/java/truffle_scheme6/builtins/symbols/SIsSymbol.java
+++ src/main/java/truffle_scheme6/builtins/symbols/SIsSymbol.java
@@ -1,10 +1,11 @@
-package truffle_scheme6.builtins;
+package truffle_scheme6.builtins.symbols;
 
 import com.oracle.truffle.api.dsl.NodeChild;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.dsl.TypeSystemReference;
 import truffle_scheme6.SchemeNode;
 import truffle_scheme6.annotations.BuiltinInfo;
+import truffle_scheme6.builtins.SBuiltin;
 import truffle_scheme6.nodes.STypesStrong;
 import truffle_scheme6.runtime.SSymbol;
 
