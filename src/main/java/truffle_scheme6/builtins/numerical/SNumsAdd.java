f30e6b5241c33ea0c28c4fb59745180102958aea
Denis Perdomo <joanp307@gmail.com>
2026-01-26 23:25:33 -0500

    fixed `+` after changing variadic args into lists instead of arrays

--- src/main/java/truffle_scheme6/builtins/numerical/SNumsAdd.java
+++ src/main/java/truffle_scheme6/builtins/numerical/SNumsAdd.java
@@ -6,7 +6,13 @@ import com.oracle.truffle.api.interop.UnsupportedTypeException;
 import truffle_scheme6.SchemeNode;
 import truffle_scheme6.annotations.BuiltinInfo;
 import truffle_scheme6.builtins.SBuiltin;
+import truffle_scheme6.runtime.SList;
+import truffle_scheme6.runtime.SNil;
+import truffle_scheme6.runtime.SPair;
 import truffle_scheme6.utils.StaticUtils;
+
+import java.util.Arrays;
+import java.util.List;
 
 @BuiltinInfo(name = "+", lastVarArgs = true)
 @NodeChild(value = "args", type = SchemeNode.class)
@@ -15,23 +21,33 @@ public abstract class SNumsAdd extends SBuiltin {
     private UBinaryAddition adder = UBinaryAdditionNodeGen.create();
 
     @Specialization
-    public Object doObjectArr(Object[] args) {
-        if (args.length == 0) {
-            return 0;
-        } else if (args.length == 1) {
-            if (StaticUtils.isNumber(args[0])) {
-                return args[0];
-            } else {
-                throw new RuntimeException(UnsupportedTypeException.create(args, "Value given is not a valid number"));
-            }
-        } else {
-            var result = args[0];
+    public Object doPair(SList args) {
+        return switch (args) {
+            case SNil _nil -> 0;
+            case SPair pair -> {
+                var car = pair.getCar();
+                if (!StaticUtils.isNumber(car)) { // validating in case the list only has one element (node wouldn't run and therefore wouldn't check)
+                    throw new RuntimeException(UnsupportedTypeException.create(pair.toArray(), "Value given is not a valid number: " + car + " of type " + car.getClass() + " within " + pair));
+                }
+                var a = new Object[]{pair.iterator()};
 
-            for (int i = 1; i < args.length; i++) {
-                result = adder.execute(result, args[i]);
-            }
+                var result = car;
+                System.out.println("Result init: " + result);
+                SPair node = pair;
+                System.out.println("Node init: " + node);
+                while (node.getCdr() instanceof SPair) {
+                    node = (SPair) (node.getCdr());
+                    car = node.getCar();
+                    System.out.println("car loop:" + car);
+                    System.out.println("Result loop: " + result);
+                    System.out.println("Node loop: " + node);
+                    result = adder.execute(result, car);
+                    System.out.println("Result loop end: " + result);
+                }
 
-            return result;
-        }
+                yield result;
+            }
+            default -> throw new IllegalArgumentException("Invalid args");
+        };
     }
 }
